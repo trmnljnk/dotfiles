e3545acb371d0022766663a7d1daa79e7de2957a
trmnljnk <trmnljnk@users.noreply.github.com>
2021-08-12 18:20:18 +0200

    Update dockutil.sh

--- dockutil.sh
+++ dockutil.sh
@@ -1,3 +1,23 @@
 
+'App Store'
+'TV'
+'Launchpad'
+'Reminders'
+'Photos'
+'Music'
+
+
+
+
+
+
+Brave Browser.app
+Firefox.app
+KeePassX.app
+KeePassXC.app
+Visual Studio Code.app 
+
+
 
 dockutil --add ~/Applications/Firefox.app --replacing 'App Store'
+dockutil --add ~/Applications/KeepassXC.app --replacing 'Brave-Browser'
