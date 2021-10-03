95b4ddd494e9351e72063cc0eca2cda014ee6c19
trmnljnk <trmnljnk@users.noreply.github.com>
2021-10-03 13:59:36 +0200

    Update bundle.sh

--- bundle.sh
+++ bundle.sh
@@ -3,4 +3,4 @@
 
 printf "\n%s\n\n" "===== $(date +%Y-%m-%d-%H%M%S) ====" >> ${HOME}/bin/brewInstallation.log
 
-brew bundle --verbose | tee >> ${HOME}/bin/brewInstallation.log
+brew bundle --verbose --file=~/bin/Brewfile  | tee >> ${HOME}/bin/brewInstallation.log
