4ca3986596c84f30009df6657ad7d0a3958d7168
trmnljnk <trmnljnk@users.noreply.github.com>
2024-03-25 00:15:02 +0400

    Create init.el
    Added init.el.

---
+++ init.el
@@ -0,0 +1,30 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.
+;; See `package-archive-priorities` and `package-pinned-packages`.
+;; Most users will not need or want to do this.
+;; (add-to-list 'package-archives
+;;              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(autoload 'ledger-mode "ledger-mode" "A major mode for Ledger" t)
+     (add-to-list 'load-path
+                  (expand-file-name "/path/to/ledger/source/lisp/"))
+     (add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(ledger-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(set-face-attribute 'default nil :height 140)
